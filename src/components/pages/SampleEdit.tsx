595b06d86d4d63ca484e62c3711695b6011d36f2
David Velasco <vdevelasko@gmail.com>
2024-08-16 08:44:29 +0200

    feat: added simple sample edit sketch

--- src/components/pages/SampleEdit.tsx
+++ src/components/pages/SampleEdit.tsx
@@ -6,9 +6,24 @@ const SampleEdit = () => {
         justifyContent: "centre",
         alignItems: "centre",
         height: "100vh",
+        width: "100vw",
       }}
     >
-      <h1>Sample Editing Page</h1>
+      <div className="flex items-top mt-0">
+        <div>
+          <div className=" w-full h-full ">
+            <img src="http://placehold.it/800x300"></img>
+            <div className="flex flex-row p-2">
+              <button className="m-2">Play</button>
+              <button className="m-2">Stop</button>
+              <button className="m-2">Toggle</button>
+              <div className="m-2">Speed</div>
+              <div className="m-2">Pitch</div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
